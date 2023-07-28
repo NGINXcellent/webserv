ff19efb1380b4e6f82cdc1f74cf0980994ff0613
Daniel Vargas <daniel_v93@hotmail.com>
2023-07-28 19:00:09 -0300

    Epoll Events inserted
    Epoll events to handle Fd state
    it can be
    EPOLLIN -> ready to receive Data
    EPOLLOUT -> ready to write Data
    EPOLLRDHUP-> lost conection to client

--- src/socketClass.cpp
+++ src/socketClass.cpp
@@ -6,7 +6,7 @@
 /*   By: dvargas <dvargas@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 08:40:52 by dvargas           #+#    #+#             */
-/*   Updated: 2023/07/28 10:11:38 by dvargas          ###   ########.fr       */
+/*   Updated: 2023/07/28 18:58:00 by dvargas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,9 @@ int TCPServerSocket::bindAndListen()
     }
 
     // Bind sockfd on epoll event
-    struct epoll_event event;
-    event.events = EPOLLIN;
-    event.data.fd = sockfd;
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd, &event) == -1)
+    events->events = EPOLLIN;
+    events->data.fd = sockfd;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sockfd, events) == -1)
     {
         std::cerr << "Failed to add sockfd to epoll. errno: " << errno << std::endl;
         exit(EXIT_FAILURE);
@@ -73,10 +72,10 @@ void TCPServerSocket:: addNewConnection(){
     }
     else {
         // try to add to epoll
-        struct epoll_event event;
-        event.events = EPOLLIN;
-        event.data.fd = newConnection;
-        if (epoll_ctl(epollfd, EPOLL_CTL_ADD, newConnection, &event) == -1) {
+        // // EPOLLOUT is not implemented yet
+        events->events = EPOLLIN | EPOLLRDHUP | EPOLLOUT;
+        events->data.fd = newConnection;
+        if (epoll_ctl(epollfd, EPOLL_CTL_ADD, newConnection, events) == -1) {
             std::cerr << "Failed to add new connection to epoll. errno: " << errno << std::endl;
             close(newConnection);
         }
@@ -96,64 +95,76 @@ void TCPServerSocket::handleConnections() {
             std::cerr << "epoll_wait error. errno: " << errno << std::endl;
             continue;
         }
-
         for (int i = 0; i < numEvents; ++i) {
-            int currentFd = events[i].data.fd;
+            int currentFd = events[i].data.fd; //this client fd
+            int currentEvent = events[i].events; // this client event state
             // in the future, we will iterate thru servers(sockfd) so the nextline will be a funcion.
             if (currentFd == sockfd) {
                 // found new conection, try to add connection
                 addNewConnection();
-                std::cout << "new conection from fd:" << currentFd << std::endl;
+                std::cout << "new conection"<< std::endl;
                 }
-            else {
+            else if ((currentEvent & EPOLLRDHUP) == EPOLLRDHUP) {
+                epoll_ctl(epollfd, EPOLL_CTL_DEL, currentFd, NULL);
+                std::cout << "Connection with FD -> " << currentFd << " is closed by client" << std::endl;
+                for (std::vector<int>::iterator it = connections.begin(); it != connections.end(); ++it)
+                {
+                    if (*it == currentFd) {
+                        connections.erase(it);
+                        break;
+                    }
+                }
+            }
+            else if ((currentEvent & EPOLLIN) == EPOLLIN) {
                 // If connection already exist
                 char buffer[1024];
+                bzero(buffer, sizeof(buffer));
                 int bytesRead = read(currentFd, buffer, sizeof(buffer));
                 if (bytesRead <= 0) {
                     // Connection error or close, we remove from epoll
                     // signal(SIGINT, handleSIGINT); localtion is here?
                     epoll_ctl(epollfd, EPOLL_CTL_DEL, currentFd, NULL);
                     close(currentFd);
-                    for (std::vector<int>::iterator it = connections.begin(); it != connections.end(); ++it)
-                    {
+                    for (std::vector<int>::iterator it = connections.begin(); it != connections.end(); ++it) {
                         if (*it == currentFd) {
                             connections.erase(it);
                             break;
                         }
                     }
-                }  else {
-                    // input e output handlers
+                }
+                else {
+                    // the idea here is to have an request handler to deal with requests
                     std::cout << "Received data from fd " << currentFd << ": " << buffer;
-                    memset(buffer, 0, sizeof(buffer));
                     std::string response = "Good talking to you fd\n";
                     sendData(currentFd, response.c_str(), response.size());
                 }
             }
+            // // EPOLLOUT means this FD is ready to write, so, is like while true full.
+            // else if ((currentEvent & EPOLLOUT) == EPOLLOUT) {
+            //         std::string response = "Good talking to you fd\n";
+            //         sendData(currentFd, response.c_str(), response.size());
+            // }
         }
     }
 }
 
-int TCPServerSocket::receiveData(int connection, char *buffer, int bufferSize)
-{
+int TCPServerSocket::receiveData(int connection, char *buffer, int bufferSize) {
     int bytesRead = read(connection, buffer, bufferSize);
     return bytesRead;
 }
 
 // send data to conection fd
-void TCPServerSocket::sendData(int connection, const char *data, int dataSize)
-{
+void TCPServerSocket::sendData(int connection, const char *data, int dataSize) {
     send(connection, data, dataSize, 0);
 }
 
 // close conection fd
-void TCPServerSocket::closeConnection(int connection)
-{
+void TCPServerSocket::closeConnection(int connection) {
     close(connection);
 }
 
 // simple destructor
-TCPServerSocket::~TCPServerSocket()
-{
+TCPServerSocket::~TCPServerSocket() {
     if (epollfd != -1) {
         close(epollfd);
     }
