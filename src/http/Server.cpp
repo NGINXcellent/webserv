05f357dfab9109004da54cf995ff5440a57ecbc0
Leonardo Farias <leofariasrj25@gmail.com>
2023-08-12 20:21:33 -0300

    Refactor: head now uses stat and has its own internal logic

--- src/http/Server.cpp
+++ src/http/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: dvargas <dvargas@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:22:33 by lfarias-          #+#    #+#             */
-/*   Updated: 2023/08/12 19:21:06 by lfarias-         ###   ########.fr       */
+/*   Updated: 2023/08/12 19:57:50 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,9 +126,42 @@ void Server::get(HttpRequest *request, HttpResponse *response) {
 }
 
 void Server::head(HttpRequest *request, HttpResponse *response) {
-  this->get(request, response);
-  std::vector<char> empty;
-  response->setMsgBody(empty);
+  int protoMain = request->getProtocolMainVersion();
+  int protoSub = request->getProtocolSubVersion();
+  response->setProtocol("HTTP", protoMain, protoSub);
+
+  int opStatus = 0;
+  if (access(request->getResource().c_str(), F_OK) == -1) {
+    opStatus = 404;
+  } else if (access(request->getResource().c_str(), R_OK) == -1) {
+    opStatus = 403;
+  }
+
+  if (opStatus != 0) {
+    HttpResponseComposer::buildErrorResponse(response, opStatus, error_pages, \
+                                             request->getProtocolMainVersion(), \
+                                             request->getProtocolSubVersion());
+    return;
+  }
+
+  std::string modifiedTimestmap = request->getModifiedTimestampCheck();
+
+  if (!HttpTime::isModifiedSince(modifiedTimestmap, request->getResource())) {
+    response->setStatusCode(304);
+    return;
+  }
+
+  response->setLastModifiedTime(HttpTime::getLastModifiedTime(request->getResource()));
+  struct stat fileInfo;
+  off_t fileSize;
+
+  if (stat(request->getResource().c_str(), &fileInfo) == 0) {
+    fileSize = fileInfo.st_size;
+  }
+
+  response->setStatusCode(200);
+  response->setContentType(MimeType::identify(request->getResource()));
+  response->setContentLength(fileSize);
 }
 
 void Server::del(HttpRequest *request, HttpResponse *response) {
